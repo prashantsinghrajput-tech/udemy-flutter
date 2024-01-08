d5a5604dae0ad2df40921a51e20fb9a997c1ffc6
prashantsinghrajput-tech <prashantsinghkaramana@gmail.com>
2024-01-08 18:58:20 +0530

    Home Screen (Home page app bar)

--- e_commerce_app/lib/features/shop/screens/home/home.dart
+++ e_commerce_app/lib/features/shop/screens/home/home.dart
@@ -1,4 +1,8 @@
+import 'package:e_commerce_app/common/widgets/appbar/appbar.dart';
 import 'package:e_commerce_app/common/widgets/common_shapes/container/primary_header_container.dart';
+import 'package:e_commerce_app/common/widgets/products/cart/cart_menu_icon.dart';
+import 'package:e_commerce_app/utils/constants/colors.dart';
+import 'package:e_commerce_app/utils/constants/text_strings.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,13 +10,37 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
           child: Column(
         children: [
           TPrimaryHeaderContainer(
             child: Column(
-              children: [],
+              children: [
+                TAppBar(
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(TTexts.homeAppbarTitle,
+                          style: Theme.of(context)
+                              .textTheme
+                              .labelMedium!
+                              .apply(color: TColors.grey)),
+                      Text(TTexts.homeAppbarSubTitle,
+                          style:
+                              Theme.of(context).textTheme.headlineSmall!.apply(
+                                    color: TColors.white,
+                                  )),
+                    ],
+                  ),
+                  actions: [
+                    TCartCounterIcon(
+                      onPressed: () {},
+                      iconColor: TColors.white,
+                    )
+                  ],
+                )
+              ],
             ),
           )
         ],
